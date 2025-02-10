d00d5b2136c2ab8746867eb6252cd825bad7d684
Mihir <aminvasudev6@gmail.com>
2025-02-10 20:40:25 +0530

    initial files

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="navigation">
+        <div className="leftContainer">
+          <div className="leftHalf">
+            <img src="/hackscriptLogo/Black-Transparent.png" alt="Logo" />
+          </div>
+          <div className="rightHalf">
+
+          </div>
+        </div>
+        <div className="rightContainer">
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
